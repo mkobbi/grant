a14279c3d75ce05c90fff87634542ed962c3bcb3
mkobbi <mahmoud.kobbi@hotmail.fr>
2018-01-31 22:04:18 +0100

    Implementing feature_extractor

--- submissions/starting_kit/feature_extractor.py
+++ submissions/starting_kit/feature_extractor.py
@@ -2,19 +2,33 @@
 
 from __future__ import unicode_literals
 
+import urllib
+
+import nltk
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 
-def document_preprocessor(doc):
+def stopword_loader(
+        url="https://raw.githubusercontent.com/mkobbi/subvention-status-datacamp/master/data/stopwords-filter-fr.txt"):
+    try:
+        stopwords = urllib.urlopen(url).read().decode("utf-8").upper()
+        stopwords = stopwords.split('\n')
+    except IOError:
+        print('Failed to open "%s".', url)
+    return stopwords
+
+
+def document_preprocessor(sentence):
     """ A custom document preprocessor
 
     This function can be edited to add some additional
     transformation on the documents prior to tokenization.
 
-    At present, this function passes the document through
-    without modification.
     """
-    return doc
+    stopwords = stopword_loader()
+    stemmer = nltk.stem.snowball.FrenchStemmer()
+
+    return sentence
 
 
 def token_processor(tokens):
@@ -25,6 +39,13 @@ def token_processor(tokens):
 
     At present, this function just passes the tokens through.
     """
+    stemmer = nltk.stem.snowball.FrenchStemmer()
+    return list((filter(lambda x: x.lower() not in stopwords and
+                                  x.lower() not in punctuation,
+                        [stemmer.stem(t.lower())
+                         for t in word_tokenize(sentence)
+                         if t.isalpha()])))
+
     for t in tokens:
         yield t
 
@@ -47,6 +68,17 @@ class FeatureExtractor(TfidfVectorizer):
             a DataFrame, where the text data is stored in the ``statement``
             column.
         """
+        X_df = X_df.rename(columns=lambda x: x.decode('utf-8').encode('ascii', errors='ignore'))
+        string_columns = ["Nom du partenaire", 'Intitul de la demande']
+        to_drop_columns = ["Anne", "Siret", "N SIMPA", 'CP-Adresse-Libell voie', "CP-Adresse-Ville"]
+        str_categorical_columns = ["Nom du partenaire", "Appel  projets", "Appel  projets PolVille"]
+        num_categorical_columns = ["Anne", "CP-Adresse-Code postal"]
+        X_df = X_df.fillna(value=0, axis='columns')
+        X_df[string_columns] = X_df[string_columns].apply(
+            lambda x: x.str.upper().str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8'))
+        X_df[str_categorical_columns] = X_df[str_categorical_columns].apply(lambda x: x.astype('category').cat.codes)
+        X_df[num_categorical_columns] = X_df[num_categorical_columns].apply(lambda x: x.astype('int'))
+        X_df = X_df.drop(to_drop_columns, axis='columns')
         super(FeatureExtractor, self).fit(X_df.statement)
         return self
 
