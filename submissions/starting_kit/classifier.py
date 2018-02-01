aa87f9aec3edd4e8956366f745c91f29fdacdea0
mkobbi <mahmoud.kobbi@hotmail.fr>
2018-02-01 22:32:57 +0100

    Everything but problem.py works correctly

--- submissions/starting_kit/classifier.py
+++ submissions/starting_kit/classifier.py
@@ -6,3 +6,12 @@ from sklearn.ensemble import RandomForestClassifier
 class Classifier(BaseEstimator):
     def __init__(self):
         self.clf = RandomForestClassifier()
+
+    def fit(self, X, y):
+        self.clf.fit(X.todense(), y)
+
+    def predict(self, X):
+        return self.clf.predict(X.todense())
+
+    def predict_proba(self, X):
+        return self.clf.predict_proba(X)
